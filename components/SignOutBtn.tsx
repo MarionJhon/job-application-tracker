4631adc3233f15d9fc112ba0dbe41a8ccee7e889
Marion Jhon Agapay <marionagapay2001@gmail.com>
2026-01-30 23:20:44 +0800

    feat: sign out button component

---
+++ components/SignOutBtn.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { signOut } from "@/lib/auth/auth-client";
+import { DropdownMenuItem } from "./ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+
+const SignOutBtn = () => {
+  const router = useRouter();
+
+  return (
+    <DropdownMenuItem
+      onClick={async () => {
+        const result = await signOut();
+        if (result.data) {
+          router.push("/sign-in");
+        } else {
+          alert("Error signing out");
+        }
+      }}
+    >
+      Log Out
+    </DropdownMenuItem>
+  );
+};
+
+export default SignOutBtn;
